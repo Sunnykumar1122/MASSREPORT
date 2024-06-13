d3830d4441da2ef15f34f5d39ff195ace7ceddd1
BABY ROLEX <167498105+Sunnykumar1122@users.noreply.github.com>
2024-06-13 17:34:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,7 +12,7 @@ async def handle_start(bot:Client, message:Message):
     Btn = [
         [InlineKeyboardButton(text='⛑️𝖧𝖤𝖫𝖯🚁', callback_data='help'), InlineKeyboardButton(text='🌀𝖡𝖮𝖳 𝖲𝖳𝖠𝖳𝖴𝖲✳️', callback_data='server')],
         [InlineKeyboardButton(text='📰𝖴𝖯𝖣𝖠𝖳𝖤🗞️', url='https://t.me/DEMON_BOT_UPDATE'), InlineKeyboardButton(text='🤖𝖡𝖮𝖳 𝖨𝖭𝖥𝖮ℹ️', callback_data='about')],
-        [InlineKeyboardButton(text='🧑‍💻ROLEX⌨️', url='https://t.me/ll_ROLEX_lll)]
+        [InlineKeyboardButton(text='🧑‍💻ROLEX⌨️', url='https://t.me/ll_ROLEX_lll')]
         ]
 
     await message.reply_text(text=Txt.START_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup(Btn))
